d0d171a789454089f8c6f585a629c9a950b64a0e
guillaume-chevalier <guillaume.chevalier@neuraxio.com>
2020-01-16 01:01:02 -0500

    Cleaned code

--- seq2seq.py
+++ seq2seq.py
@@ -1,6 +1,5 @@
-from typing import Callable
+from typing import List
 
-import numpy as np
 import tensorflow as tf
 from neuraxle.data_container import DataContainer
 from neuraxle.hyperparams.space import HyperparameterSamples
@@ -17,21 +16,19 @@ from tensorflow_core.python.keras.layers import GRUCell, RNN, Dense
 from tensorflow_core.python.training.adam import AdamOptimizer
 
 from datasets import generate_data
+from datasets import metric_3d_to_2d_wrapper
 from neuraxle_tensorflow.tensorflow_v1 import TensorflowV1ModelStep
 from neuraxle_tensorflow.tensorflow_v2 import Tensorflow2ModelStep
 from plotting import plot_metrics
 from steps import MeanStdNormalizer, ToNumpy, PlotPredictionsWrapper
 
 
-def create_model(step: Tensorflow2ModelStep):
+def create_model(step: Tensorflow2ModelStep) -> tf.keras.Model:
     """
-    Create tensorflow 2 encoder decoder sequence to sequence model.
+   Create a TensorFlow v2 sequence to sequence (seq2seq) encoder-decoder model.
 
-    :param step: tensorflow 2 model step
-    :type step: Tensorflow2ModelStep
-
-    :return: tensorflow 2 keras model
-    :rtype: tf.keras.Model
+   :param step: The base Neuraxle step for TensorFlow v2 (Tensorflow2ModelStep)
+   :return: TensorFlow v2 Keras model
     """
     # shape: (batch_size, seq_length, input_dim)
     encoder_inputs = Input(
@@ -47,17 +44,15 @@ def create_model(step: Tensorflow2ModelStep):
     return Model(encoder_inputs, decoder_outputs)
 
 
-def _create_encoder(step: Tensorflow2ModelStep, encoder_inputs):
+def _create_encoder(step: Tensorflow2ModelStep, encoder_inputs: Input) -> (tf.Tensor, List[tf.Tensor]):
     """
-    Create encoder RNN.
+   Create an encoder RNN using GRU Cells. GRU cells are similar to LSTM cells.
 
-    :param step: tensorflow 2 model step
-    :type step: Tensorflow2ModelStep
+   :param step: The base Neuraxle step for TensorFlow v2 (class Tensorflow2ModelStep)
     :param encoder_inputs: encoder inputs layer of shape (batch_size, seq_length, input_dim)
     :return: (last encoder outputs, last stacked encoders states)
                 last_encoder_outputs shape: (batch_size, hidden_dim)
                 last_encoder_states shape: (layers_stacked_count, batch_size, hidden_dim)
-    :rtype: Tuple[tf.Tensor, [tf.Tensor]]
     """
     encoder = RNN(cell=_create_stacked_rnn_cells(step), return_sequences=False, return_state=True)
 
@@ -70,19 +65,16 @@ def _create_encoder(step: Tensorflow2ModelStep, encoder_inputs):
     return last_encoder_outputs, last_encoders_states
 
 
-def _create_decoder(step: Tensorflow2ModelStep, last_encoder_outputs, last_encoders_states):
+def _create_decoder(
+        step: Tensorflow2ModelStep, last_encoder_outputs: tf.Tensor, last_encoders_states: List[tf.Tensor]
+) -> tf.Tensor:
     """
-    Create decoder RNN.
+   Create a decoder RNN using GRU cells.
 
-    :param step: tensorflow 2 model step
-    :type step: Tensorflow2ModelStep
+   :param step: The base Neuraxle step for TensorFlow v2 (Tensorflow2ModelStep)
     :param last_encoders_states: last encoder states tensor
-    :type last_encoders_states: tf.Tensor
     :param last_encoder_outputs: last encoder output tensor
-    :type last_encoder_outputs: tf.Tensor
-
     :return: decoder output
-    :rtype: tf.Tensor
     """
     decoder_lstm = RNN(cell=_create_stacked_rnn_cells(step), return_sequences=True, return_state=False)
 
@@ -100,16 +92,18 @@ def _create_decoder(step: Tensorflow2ModelStep, last_encoder_outputs, last_encod
     # decoder outputs shape: (batch_size, window_size_future, hidden_dim)
 
     decoder_dense = Dense(step.hyperparams['output_dim'])
+    # decoder outputs shape: (batch_size, window_size_future, output_dim)
 
     return decoder_dense(decoder_outputs)
 
 
-def _create_stacked_rnn_cells(step: Tensorflow2ModelStep):
+def _create_stacked_rnn_cells(step: Tensorflow2ModelStep) -> List[GRUCell]:
     """
-    Create layers_stacked_count stacked GRU cells with hidden_dim units.
+   Create a `layers_stacked_count` amount of GRU cells and stack them on top of each other.
+   They have a `hidden_dim` number of neuron layer size.
 
+   :param step: The base Neuraxle step for TensorFlow v2 (Tensorflow2ModelStep)
     :return: list of gru cells
-    :rtype: List[GRUCell]
     """
     cells = []
     for _ in range(step.hyperparams['layers_stacked_count']):
@@ -118,19 +112,14 @@ def _create_stacked_rnn_cells(step: Tensorflow2ModelStep):
     return cells
 
 
-def create_loss(step: Tensorflow2ModelStep, expected_outputs, predicted_outputs):
+def create_loss(step: Tensorflow2ModelStep, expected_outputs: tf.Tensor, predicted_outputs: tf.Tensor) -> tf.Tensor:
     """
     Create model loss.
 
-    :param step: tensorflow 2 model step
-    :type step: Tensorflow2ModelStep
-    :param expected_outputs: expected outputs
-    :type expected_outputs: (batch_size, window_size_future, output_dim)
-    :param predicted_outputs: expected outputs
-    :type predicted_outputs: (batch_size, window_size_future, output_dim)
-
-    :return: loss
-    :rtype: float
+   :param step: The base Neuraxle step for TensorFlow v2 (Tensorflow2ModelStep)
+   :param expected_outputs: expected outputs of shape (batch_size, window_size_future, output_dim)
+   :param predicted_outputs: expected outputs of shape (batch_size, window_size_future, output_dim)
+   :return: loss (a tf Tensor that is a float)
     """
     l2 = step.hyperparams['lambda_loss_amount'] * sum(
         tf.reduce_mean(tf.nn.l2_loss(tf_var))
@@ -145,34 +134,22 @@ def create_loss(step: Tensorflow2ModelStep, expected_outputs, predicted_outputs)
     return output_loss + l2
 
 
-def create_optimizer(step: TensorflowV1ModelStep):
+def create_optimizer(step: TensorflowV1ModelStep) -> AdamOptimizer:
     """
-    Create tensorflow 2 optimizer.
+   Create a TensorFlow 2 Optimizer: here the AdamOptimizer.
 
-    :param step: tensorflow 2 model step
-    :type step: Tensorflow2ModelStep
-
+   :param step: The base Neuraxle step for TensorFlow v2 (Tensorflow2ModelStep)
     :return: optimizer
-    :rtype: tensorflow 2 optimizer.Optimizer
     """
     return AdamOptimizer(learning_rate=step.hyperparams['learning_rate'])
-
-
-def metric_3d_to_2d_wrapper(metric_fun: Callable):
-    def metric(data_inputs, expected_outputs):
-        # We keep axis 0 for evaluation on USD only (or on first dim only when we have multidim output).
-        return metric_fun(np.array(data_inputs)[..., 0], np.array(expected_outputs)[..., 0])
-
-    return metric
 
 
 def main():
     exercice_number = 1
     print('exercice {}\n=================='.format(exercice_number))
-    tf.debugging.set_log_device_placement(True)
-    print('You can use the following devices: {}'.format(get_avaible_devices()))
 
     data_inputs, expected_outputs = generate_data(
+        # See: https://github.com/guillaume-chevalier/seq2seq-signal-prediction/blob/master/datasets.py
         exercice_number=exercice_number,
         n_samples=None,
         window_size_past=None,
@@ -187,7 +164,7 @@ def main():
     output_dim = expected_outputs.shape[2]
 
     batch_size = 100
-    epochs = 2
+    epochs = 3
     validation_size = 0.15
     max_plotted_validation_predictions = 10
 
@@ -207,12 +184,13 @@ def main():
         ForEachDataInput(MeanStdNormalizer()),
         ToNumpy(),
         PlotPredictionsWrapper(Tensorflow2ModelStep(
-                create_model=create_model,
-                create_loss=create_loss,
-                create_optimizer=create_optimizer,
-                expected_outputs_dtype=tf.dtypes.float32,
-                data_inputs_dtype=tf.dtypes.float32,
-                print_loss=True
+            # See: https://github.com/Neuraxio/Neuraxle-TensorFlow
+            create_model=create_model,
+            create_loss=create_loss,
+            create_optimizer=create_optimizer,
+            expected_outputs_dtype=tf.dtypes.float32,
+            data_inputs_dtype=tf.dtypes.float32,
+            print_loss=True
         ).set_hyperparams(seq2seq_pipeline_hyperparams))
     ]).set_name('SignalPrediction')
 
@@ -255,10 +233,9 @@ def plot_predictions(data_inputs, expected_outputs, pipeline, max_plotted_predic
     ))
 
 
-def get_avaible_devices():
-    return [x.name for x in device_lib.list_local_devices()]
-
-
 if __name__ == '__main__':
+    tf.debugging.set_log_device_placement(True)
+    print('You can use the following tf devices: {}'.format(
+        [x.name for x in device_lib.list_local_devices()]))
+
     main()
-    # TODO: how to import external code files: https://stackoverflow.com/questions/48905127/importing-py-files-in-google-colab
